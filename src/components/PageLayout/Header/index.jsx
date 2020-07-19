b87d5823ea9b2729587dcf9ee1c19c2545a1e5ae
cesar <csgalindos@hotmail.com>
2020-07-19 17:14:29 -0500

    [updated] without blog

--- src/components/PageLayout/Header/index.jsx
+++ src/components/PageLayout/Header/index.jsx
@@ -36,31 +36,6 @@ export default () => {
                 About
               </Link>
             </li>
-            {/* <li className={style.navItem}>
-              <Link to="/contact" onClick={toggleMenu} activeClassName={style.anchorActive}>
-                Contact
-              </Link>
-            </li> */}
-            <li className={style.navItem}>
-              <Link to="/blog" onClick={toggleMenu} activeClassName={style.anchorActive}>
-                Blog
-              </Link>
-            </li>
-            <li className={style.navItem}>
-              <Link to="/tags" onClick={toggleMenu} activeClassName={style.anchorActive}>
-                Tags
-              </Link>
-            </li>
-            <li className={style.navItem}>
-              <Link to="/portafolio" onClick={toggleMenu} activeClassName={style.anchorActive}>
-                Portafolio
-              </Link>
-            </li>
-            {/* <li className={style.navItem}>
-              <Link to="/resume" onClick={toggleMenu} activeClassName={style.anchorActive}>
-                Resume
-              </Link>
-            </li> */}
           </ul>
         </div>
       </Layout>
